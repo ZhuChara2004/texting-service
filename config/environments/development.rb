e566e280f54c0a9d228d4e47c929563b2354e9a4
Dmytro Zhuk <zhuchara@gmail.com>
2023-08-11 19:49:59 +0300

    Allow ngrok to work in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # allow ngrok to work
+  config.hosts << /[a-z0-9\-.]+\.ngrok\.io/
+  config.hosts << /[a-z0-9\-.]+\.ngrok\.dev/
 end
